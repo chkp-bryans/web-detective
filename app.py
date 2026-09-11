e40be03fee25e38bcabede08470f02803f748927
Bryan Smith <97714585+chkp-bryans@users.noreply.github.com>
2026-09-11 12:02:29 -0400

    Release 1.2.3: bump app version

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ AUTH_USER = os.environ.get("BASIC_AUTH_USER", "")
 AUTH_PASSWORD = os.environ.get("BASIC_AUTH_PASSWORD", "")
 ALLOW_UNAUTHENTICATED = os.environ.get("DETECTIVE_ALLOW_UNAUTHENTICATED", "") == "1"
 WAFBUDDY_URL = os.environ.get("WAFBUDDY_URL", "https://wafbuddy.csadocs.com")
-APP_RELEASE = os.environ.get("RELEASE") or os.environ.get("APP_RELEASE") or "1.2.2"
+APP_RELEASE = os.environ.get("RELEASE") or os.environ.get("APP_RELEASE") or "1.2.3"
 
 
 def _authorized() -> bool:
